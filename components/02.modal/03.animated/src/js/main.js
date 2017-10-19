f516773bdfec0131aa89c9f8f2264cb77567e194
Zell Liew <zellwk@gmail.com>
2017-10-19 18:47:13 +0800

    correct model starter files
    Signed-off-by: Zell Liew <zellwk@gmail.com>

--- components/02.modal/03.animated/src/js/main.js
+++ components/02.modal/03.animated/src/js/main.js
@@ -1,12 +1,31 @@
+/* globals TimelineMax Back */
+import 'gsap'
+
 // # START EDITING YOUR JAVASCRIPT HERE
 // ===============
 
+// Wave animation
+const wave = hand => {
+  const tl = new TimelineMax({})
+  tl.set(hand, {transformOrigin: 'bottom center'})
+  tl.from(hand, 0.5, {scale: 0.25, autoAlpha: 0, ease: Back.easeOut.config(1.5)})
+  tl.to(hand, 0.25, {rotation: -15}, '+=0.05')
+  tl.to(hand, 0.25, {rotation: 15})
+  tl.to(hand, 0.25, {rotation: -15})
+  tl.to(hand, 0.25, {rotation: 0})
+}
+
+// The event listeners
 const toggleButton = document.querySelector('.jsModalToggle')
 const closeButton = document.querySelector('.jsModalClose')
 const overlay = document.querySelector('.jsModalContainer')
 const modal = document.querySelector('.jsModal')
+const hand = modal.querySelector('.jsWaveHand')
 
-const openModal = _ => document.body.classList.add('modal-is-open')
+const openModal = _ => {
+  document.body.classList.add('modal-is-open')
+  wave(hand)
+}
 const closeModal = _ => document.body.classList.remove('modal-is-open')
 
 toggleButton.addEventListener('click', openModal)
