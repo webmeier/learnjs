cd5080a3b76928afc02f02eedb31ae0d030b6177
Zell Liew <zellwk@gmail.com>
2017-12-23 21:54:56 +0800

    remove webpack from tabs
    Signed-off-by: Zell Liew <zellwk@gmail.com>

--- components/04.tabs/02.basic-completed/js/main.js
+++ components/04.tabs/02.basic-completed/js/main.js
@@ -1,6 +1,5 @@
 // # START EDITING YOUR JAVASCRIPT HERE
 // ===============
-
 const tabList = document.querySelector('.jsTabList')
 
 tabList.addEventListener('click', e => {
@@ -21,22 +20,3 @@ tabList.addEventListener('click', e => {
   newTab.classList.add('is-active')
   newTabContent.classList.add('is-active')
 })
-
-// Enables Hot module replacement for Webpack. Leave this alone
-if (process.env.NODE_ENV === 'development') {
-  if (module.hot) module.hot.accept()
-
-  // Silence HMR rubbish logs
-  ;(function (global) {
-    var consoleLog = global.console.log
-    global.console.log = function () {
-      if (!(
-            arguments.length === 1 &&
-            typeof arguments[0] === 'string' &&
-            arguments[0].match(/^\[(HMR|WDS)\]/)
-          )) {
-        consoleLog.apply(global.console, arguments)
-      }
-    }
-  })(window)
-}
