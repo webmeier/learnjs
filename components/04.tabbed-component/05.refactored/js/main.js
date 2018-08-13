7f7f912bfda6bdb79a3ba4581cfa144cd86e48d8
Zell Liew <zellwk@gmail.com>
2018-08-13 21:02:27 +0800

    refactored tab
    Signed-off-by: Zell Liew <zellwk@gmail.com>

--- components/04.tabbed-component/05.refactored/js/main.js
+++ components/04.tabbed-component/05.refactored/js/main.js
@@ -1,23 +1,23 @@
 // # START EDITING YOUR JAVASCRIPT HERE
 // ===============
-const component = document.querySelector('.tabbed-component')
-const tabList = component.querySelector('.tabs')
-const tabs = [...component.querySelectorAll('.tab')]
-const contents = [...component.querySelectorAll('.tab-content')]
-
-const toggleTabs = e => {
+const toggleTabs = (e, component) => {
   const link = e.target.closest('a')
-  const tab = link.parentNode
   const href = link.getAttribute('href')
-  const tabContent = component.querySelector(href)
+  const selectedTab = link.parentNode
+  const selectedTabContent = component.querySelector(href)
+  const tabs = [...selectedTab.parentElement.children]
+  const tabContents = [...component.querySelectorAll('.tab-content')]
 
   // Hiding previous tab and tab content
   tabs.forEach(elem => elem.classList.remove('is-selected'))
-  contents.forEach(elem => elem.classList.remove('is-selected'))
+  tabContents.forEach(elem => elem.classList.remove('is-selected'))
 
   // Showing the selected tab and tab content
-  tab.classList.add('is-selected')
-  tabContent.classList.add('is-selected')
+  selectedTab.classList.add('is-selected')
+  selectedTabContent.classList.add('is-selected')
 }
+
+const component = document.querySelector('.tabbed-component')
+const tabList = component.querySelector('.tabs')
 
 tabList.addEventListener('click', e => toggleTabs(e, component))
