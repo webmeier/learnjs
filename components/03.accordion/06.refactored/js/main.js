9b4e002e83441c5d328f1a0944b45ee8cff379a0
Zell Liew <zellwk@gmail.com>
2018-08-13 20:29:10 +0800

    refactored accordion
    Signed-off-by: Zell Liew <zellwk@gmail.com>

--- components/03.accordion/06.refactored/js/main.js
+++ components/03.accordion/06.refactored/js/main.js
@@ -1,21 +1,32 @@
 // # START EDITING YOUR JAVASCRIPT HERE
 // ===============
 
-{
-  const toggleAccordion = e => {
-    const header = e.target.closest('.accordion__header')
-    if (!header) return
-    const accordion = header.parentElement
-    const content = header.nextElementSibling
-    const inner = content.children[0]
-    const height = accordion.classList.contains('is-open')
+const getHeight = (accordion, content) => {
+  const inner = content.children[0]
+  const height = accordion.classList.contains('is-open')
     ? 0
     : inner.getBoundingClientRect().height
 
-    content.style.height = `${height}px`
-    accordion.classList.toggle('is-open')
-  }
-
-  const container = document.querySelector('.accordion-container')
-  container.addEventListener('click', toggleAccordion)
+  return height
 }
+
+const toggleAccordion = accordion => {
+  accordion.classList.toggle('is-open')
+}
+
+const updateHeight = (elem, height) => {
+  elem.style.height = `${height}px`
+}
+
+const container = document.querySelector('.accordion-container')
+container.addEventListener('click', e => {
+  const header = e.target.closest('.accordion__header')
+  if (!header) return
+
+  const accordion = header.parentElement
+  const content = header.nextElementSibling
+  const height = getHeight(accordion, content)
+
+  updateHeight(content, height)
+  toggleAccordion(accordion)
+})
