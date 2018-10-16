4ce16b0e26d16043f2f5833be2537e8c5401ca19
Zell Liew <zellwk@gmail.com>
2018-10-16 18:43:33 +0800

    add Dota 1
    Signed-off-by: Zell Liew <zellwk@gmail.com>

---
+++ components/10.dota/02.basic/js/main.js
@@ -0,0 +1,22 @@
+/* globals DOMPurify zlFetch */
+// # START EDITING YOUR JAVASCRIPT HERE
+// ===============
+
+const dotaApi = 'https://api.opendota.com/api'
+
+const init = data => {
+  const heroesDiv = document.querySelector('.heroes')
+
+  const innerHTML = data.body.map(hero => {
+    return `<div class="hero">
+      <img src="https://api.opendota.com${hero.img}" />
+      <div class="hero__name">${hero['localized_name']}</div>
+    </div>`
+  }).join('')
+
+  heroesDiv.innerHTML = DOMPurify.sanitize(innerHTML)
+}
+
+zlFetch(`${dotaApi}/heroStats`)
+  .then(init)
+  .catch(console.log)
