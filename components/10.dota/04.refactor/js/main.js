4479823a965b95cba130e495ef12468f35afcb33
Zell Liew <zellwk@gmail.com>
2018-10-18 05:19:27 +0800

    Add refactor dota heroes
    Signed-off-by: Zell Liew <zellwk@gmail.com>

---
+++ components/10.dota/04.refactor/js/main.js
@@ -0,0 +1,76 @@
+/* globals DOMPurify zlFetch */
+// # START EDITING YOUR JAVASCRIPT HERE
+// ===============
+
+const dotaApi = 'https://api.opendota.com/api'
+
+const init = data => {
+  const heroes = data.body.map(hero => {
+    return {
+      name: hero['localized_name'],
+      primaryAttr: hero['primary_attribute'],
+      attackType: hero['attack_type'],
+      roles: hero.roles,
+      img: `https://api.opendota.com${hero.img}`
+    }
+  })
+
+  const filters = document.querySelector('.filters')
+  renderHeroes(heroes)
+  filters.addEventListener('change', _ => {
+    renderHeroes(filterHeroes(heroes))
+  })
+}
+
+const renderHeroes = heroes => {
+  const innerHTML = heroes.map(hero => {
+    return `<div class="hero">
+      <img src="${hero.img}" />
+      <div class="hero__name">${hero.name}</div>
+    </div>`
+  }).join('')
+
+  const heroesDiv = document.querySelector('.heroes')
+  heroesDiv.innerHTML = DOMPurify.sanitize(innerHTML)
+}
+
+const filterHeroes = heroes => {
+  const filter1 = filterByPrimaryAttribute(heroes)
+  const filter2 = filterByAttackType(filter1)
+  return filterByRoles(filter2)
+}
+
+const filterByPrimaryAttribute = heroes => {
+  const selectedAttributes = Array.from(
+    document.querySelector('#primary-attribute')
+      .querySelectorAll(('input:checked'))
+  ).map(el => el.id)
+
+  if (!selectedAttributes.length) return heroes
+  return heroes.filter(hero => selectedAttributes.includes(hero.primaryAttr))
+}
+
+const filterByAttackType = heroes => {
+  const selectedAttackTypes = Array.from(
+    document.querySelector('#attack-type')
+      .querySelectorAll(('input:checked'))
+  ).map(el => el.id)
+
+  if (!selectedAttackTypes.length) return heroes
+  return heroes.filter(hero => selectedAttackTypes.includes(hero.attackType))
+}
+
+const filterByRoles = heroes => {
+  const selectedRoles = Array.from(
+    document.querySelector('#roles')
+      .querySelectorAll(('input:checked'))
+  ).map(el => el.id)
+
+  return heroes.filter(hero =>
+    selectedRoles.every(selectedRole => hero.roles.includes(selectedRole))
+  )
+}
+
+zlFetch(`${dotaApi}/heroStats`)
+  .then(init)
+  .catch(console.log)
