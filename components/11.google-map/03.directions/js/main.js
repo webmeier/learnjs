27b653cb611ae040bd93145745fcfa4af8f93f64
Zell Liew <zellwk@gmail.com>
2018-11-09 17:40:57 +0800

    Add: Gmaps lesson 3 draft
    Signed-off-by: Zell Liew <zellwk@gmail.com>

--- components/11.google-map/03.directions/js/main.js
+++ components/11.google-map/03.directions/js/main.js
@@ -1,9 +1,33 @@
 /* globals google */
+/* eslint-disable no-new  */
+const places = {
+  artScienceMuseum: {
+    latlng: { lat: 1.2863, lng: 103.8593 },
+    addr: '6 Bayfront Ave, Singapore 018974'
+  },
+  gardensByTheBay: {
+    latlng: { lat: 1.2816, lng: 103.8636 },
+    addr: '18 Marina Gardens Dr, Singapore 018953'
+  },
+  littleIndia: {
+    latlng: { lat: 1.3066, lng: 103.8518 },
+    addr: 'Little India, Singapore'
+  },
+  sentosa: {
+    latlng: { lat: 1.2494, lng: 103.8303 },
+    addr: 'Sentosa, Singapore'
+  },
+  singaporeZoo: {
+    latlng: { lat: 1.4043, lng: 103.7930 },
+    addr: '80 Mandai Lake Rd, Singapore 729826'
+  }
+}
+
 // # Start editing your JavaScript here
 // ===============
 
 // Please change this to use your own API key!
-const apiKey = 'YOUR_API_KEY'
+const apiKey = 'AIzaSyBOSppjMrbl5YQAUla6O9WNAL1w2zeWtLc'
 const gmapsURI = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places`
 
 const fetchWithJSONP = (uri, callback, err = console.error) => {
@@ -23,6 +47,25 @@ function initMap () {
   const map = new google.maps.Map(mapDiv, {
     center: { lat: 1.3521, lng: 103.8198 },
     zoom: 13
+  })
+
+  const directionsService = new google.maps.DirectionsService()
+  const request = {
+    origin: places.singaporeZoo.latlng,
+    destination: places.sentosa.addr,
+    travelMode: 'DRIVING'
+  }
+
+  directionsService.route(request, (result, status) => {
+    if (status === 'OK') {
+      new google.maps.DirectionsRenderer({
+        map,
+        directions: result
+      })
+    } else {
+      console.error(status)
+      console.log(result)
+    }
   })
 }
 
