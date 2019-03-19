6e0b0ac22f35b6dec3e7ac4086c9968f3e98b29e
Zell Liew <zellwk@gmail.com>
2019-03-19 17:10:27 +0800

    🔨: Update Calculator refactor for clear key
    Signed-off-by: Zell Liew <zellwk@gmail.com>

--- components/06.calculator/03.refactored/js/main.js
+++ components/06.calculator/03.refactored/js/main.js
@@ -105,8 +105,14 @@ const updateVisualState = (key, calculator) => {
   const keyType = getKeyType(key)
   Array.from(key.parentNode.children).forEach(k => k.classList.remove('is-depressed'))
 
-  if (keyType === 'operator') key.classList.add('is-depressed')
-  if (keyType === 'clear' && key.textContent !== 'AC') key.textContent = 'AC'
+  if (keyType === 'operator') {
+    key.classList.add('is-depressed')
+  }
+
+  if (keyType === 'clear') {
+    key.textContent = 'AC'
+  }
+
   if (keyType !== 'clear') {
     const clearButton = calculator.querySelector('[data-action=clear]')
     clearButton.textContent = 'CE'
